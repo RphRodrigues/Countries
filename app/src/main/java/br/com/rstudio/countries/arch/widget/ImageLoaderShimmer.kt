0359ef04aa684d7bed6c57b75969b1afb419ca6b
Raphael Rodrigues <rodrigues.rph@gmail.com>
2024-07-12 00:56:33 -0300

    fix: detekt issue

--- app/src/main/java/br/com/rstudio/countries/arch/widget/ImageLoaderShimmer.kt
+++ app/src/main/java/br/com/rstudio/countries/arch/widget/ImageLoaderShimmer.kt
@@ -3,16 +3,18 @@ package br.com.rstudio.countries.arch.widget
 import com.facebook.shimmer.Shimmer
 import com.facebook.shimmer.ShimmerDrawable
 
+private const val ALPHA = 0.7f
+private const val DURATION = 1000L
+
 fun imageLoaderShimmerDrawable() = ShimmerDrawable().apply {
 
-  val shimmer =
-    Shimmer.AlphaHighlightBuilder()// The attributes for a ShimmerDrawable is set by this builder
-      .setDuration(1000) // how long the shimmering animation takes to do one full sweep
-      .setBaseAlpha(0.7f) //the alpha of the underlying children
-      .setHighlightAlpha(0.6f) // the shimmer alpha amount
-      .setDirection(Shimmer.Direction.LEFT_TO_RIGHT)
-      .setAutoStart(true)
-      .build()
+  val shimmer = Shimmer.AlphaHighlightBuilder() // The attributes for a ShimmerDrawable is set by this builder
+    .setDuration(DURATION) // how long the shimmering animation takes to do one full sweep
+    .setBaseAlpha(ALPHA) // the alpha of the underlying children
+    .setHighlightAlpha(ALPHA) // the shimmer alpha amount
+    .setDirection(Shimmer.Direction.LEFT_TO_RIGHT)
+    .setAutoStart(true)
+    .build()
 
   setShimmer(shimmer)
 }
