d7747fb44c8bc8f228558f35b0041698ed063c41
Raphael Rodrigues <rodrigues.rph@gmail.com>
2024-07-12 01:08:36 -0300

    feat: implement quiz layout

--- app/src/main/java/br/com/rstudio/countries/presentation/quizscreen/QuizFragment.kt
+++ app/src/main/java/br/com/rstudio/countries/presentation/quizscreen/QuizFragment.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import br.com.rstudio.countries.R
+import br.com.rstudio.countries.arch.extension.getBottomLeftColor
+import br.com.rstudio.countries.arch.extension.getBottomRightColor
 import br.com.rstudio.countries.arch.extension.goToBottomNavigationHome
 import br.com.rstudio.countries.arch.extension.setupBackPressedCallback
+import br.com.rstudio.countries.databinding.FragmentQuizBinding
 
 class QuizFragment : Fragment(R.layout.fragment_quiz) {
+
+  private lateinit var binding: FragmentQuizBinding
 
   private val callback = {
     activity.goToBottomNavigationHome()
@@ -15,7 +20,16 @@ class QuizFragment : Fragment(R.layout.fragment_quiz) {
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
+    binding = FragmentQuizBinding.bind(view)
     setupBackPressedCallback(callback)
+    loadImage("https://flagcdn.com/w320/pm.png")
+  }
+
+  private fun loadImage(url: String) = binding.apply {
+    quizFlagImageView.load(url) { drawable ->
+      quizFlagDetailsViewLeft.setBackgroundColor(drawable.getBottomLeftColor())
+      quizFlagDetailsViewRight.setBackgroundColor(drawable.getBottomRightColor())
+    }
   }
 
   companion object {
