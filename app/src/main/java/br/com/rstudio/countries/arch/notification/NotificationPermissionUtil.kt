2de9bcc23e1f7ddd012ab671b21f36563aa0a989
Raphael <rodrigues.rph@gmail.com>
2025-04-30 15:04:11 -0300

    improvement: notification permission flow

--- app/src/main/java/br/com/rstudio/countries/arch/notification/NotificationPermissionUtil.kt
+++ app/src/main/java/br/com/rstudio/countries/arch/notification/NotificationPermissionUtil.kt
@@ -1,5 +1,13 @@
 package br.com.rstudio.countries.arch.notification
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.core.app.ActivityCompat.shouldShowRequestPermissionRationale
+import androidx.core.content.ContextCompat
 import br.com.rstudio.countries.arch.prefs.AppPrefs
 import br.com.rstudio.countries.arch.util.Constants
 import timber.log.Timber
@@ -12,7 +20,12 @@ import timber.log.Timber
  *
  * @param appPrefs Abstraction over shared preferences for storing and retrieving app-specific settings.
  */
-class NotificationPermissionUtil(private val appPrefs: AppPrefs) {
+class NotificationPermissionUtil(
+  context: Context,
+  private val appPrefs: AppPrefs
+) {
+
+  private val appContext = context.applicationContext
 
   /**
    * Determines whether the app should prompt the user for notification permission again.
@@ -31,6 +44,68 @@ class NotificationPermissionUtil(private val appPrefs: AppPrefs) {
 
     Timber.tag(Constants.APP_PREFS).d("Should ask notification permission again? $shouldAsk")
     return shouldAsk
+  }
+
+  /**
+   * Determines whether the app should request the notification permission.
+   *
+   * This method checks if the `POST_NOTIFICATIONS` permission has not been granted
+   * and whether the app's internal logic (e.g., based on user behavior or elapsed time)
+   * deems it's appropriate to ask again.
+   *
+   * @return `true` if the permission is not granted and the app should ask again, `false` otherwise.
+   */
+  @SuppressLint("InlinedApi")
+  fun shouldAskedNotificationPermission() =
+    ContextCompat.checkSelfPermission(
+      appContext,
+      Manifest.permission.POST_NOTIFICATIONS
+    ) != PackageManager.PERMISSION_GRANTED && shouldAskNotificationPermissionAgain()
+
+  /**
+   * Triggers the notification permission request flow for Android 13+ (TIRAMISU).
+   *
+   * This method checks if the app is running on a compatible Android version and whether
+   * the notification permission should be requested. If the user has previously denied
+   * the permission, a rationale should be shown before redirecting to system settings.
+   *
+   * Based on the situation, this function calls a provided callback to show a custom UI
+   * (e.g., a bottom sheet) with an explanation.
+   *
+   * - If the user denied the permission once, `redirectToSettings` will be `true`.
+   * - If it's the first time asking (or the user selected "Don't ask again"),
+   *   `redirectToSettings` will be `false`.
+   *
+   * The permission request timestamp is saved to avoid re-prompting too frequently.
+   *
+   * @param activity The current Activity context used to check permission rationale.
+   * @param showNotificationJustification A callback that should show a justification UI
+   * before requesting permission. The Boolean parameter indicates whether the user should
+   * be redirected to system settings.
+   */
+  fun askNotificationPermission(
+    activity: Activity,
+    showNotificationJustification: (redirectToSettings: Boolean) -> Unit
+  ) {
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) return
+    if (!shouldAskedNotificationPermission()) return
+
+    when {
+      shouldShowRequestPermissionRationale(activity, Manifest.permission.POST_NOTIFICATIONS) -> {
+        Timber.tag(Constants.PERMISSION).d("User de nied once — show a custom dialog explaining why you need it")
+        showNotificationJustification(true)
+      }
+
+      ContextCompat.checkSelfPermission(
+        appContext,
+        Manifest.permission.POST_NOTIFICATIONS
+      ) != PackageManager.PERMISSION_GRANTED -> {
+        Timber.tag(Constants.PERMISSION).d("First time asking, or user chose \"Don't ask again\"")
+        showNotificationJustification(false)
+      }
+    }
+
+    appPrefs.setLastAskedTimeNotificationPermission()
   }
 
   companion object {
--- app/src/main/java/br/com/rstudio/countries/notification/NotificationJustificationBottomSheet.kt
+++ app/src/main/java/br/com/rstudio/countries/notification/NotificationJustificationBottomSheet.kt
@@ -1,53 +1,71 @@
 package br.com.rstudio.countries.notification
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.core.view.isVisible
 import br.com.rstudio.countries.R
 import br.com.rstudio.countries.arch.extension.ScreenName
+import br.com.rstudio.countries.arch.observability.analytics.AnalyticsEvent.BUTTON
+import br.com.rstudio.countries.arch.observability.analytics.AnalyticsEvent.CLICK
 import br.com.rstudio.countries.arch.observability.analytics.AnalyticsReport
+import br.com.rstudio.countries.arch.util.Constants
 import br.com.rstudio.countries.databinding.BottomSheetNotificationJustificationBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-import kotlin.getValue
 import org.koin.android.ext.android.inject
+import timber.log.Timber
 
 class NotificationJustificationBottomSheet : BottomSheetDialogFragment() {
 
-  lateinit var onAllowClick: () -> Unit
-  lateinit var onDismissClick: () -> Unit
-  lateinit var onViewCreate: () -> Unit
+  lateinit var onAllowPermissionListener: () -> Unit
   private var binding: BottomSheetNotificationJustificationBinding? = null
   private val analytics: AnalyticsReport by inject()
 
+  private val isRedirectSettings: Boolean
+    get() = arguments?.getBoolean(REDIRECT_SETTINGS) == true
+
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
     binding = BottomSheetNotificationJustificationBinding.inflate(inflater, container, false)
-
-    onViewCreate()
-
     return binding?.root
   }
 
+  @SuppressLint("InlinedApi")
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
 
+    if (isRedirectSettings) {
+      binding?.allowButton?.text = getString(R.string.open_settings)
+      binding?.maybeLaterTextView?.isVisible = false
+    }
+
     binding?.allowButton?.setOnClickListener {
-      onAllowClick()
+      if (isRedirectSettings) {
+        analytics.trackEvent(CLICK, mapOf(BUTTON to "open_app_settings"))
+
+        startActivity(
+          Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS).apply {
+            putExtra(Settings.EXTRA_APP_PACKAGE, context?.packageName)
+          }
+        )
+      } else {
+        analytics.trackEvent(CLICK, mapOf(BUTTON to "allow_notification"))
+        onAllowPermissionListener.invoke()
+      }
       dismiss()
     }
 
     binding?.maybeLaterTextView?.setOnClickListener {
-      onDismissClick()
+      analytics.trackEvent(CLICK, mapOf(BUTTON to "maybe_later"))
+      Timber.tag(Constants.PERMISSION).d("User declined notification permission")
       dismiss()
     }
 
     analytics.trackScreenView(ScreenName)
-  }
-
-  fun updateBottomSheet() {
-    binding?.allowButton?.text = getString(R.string.open_settings)
-    binding?.maybeLaterTextView?.isVisible = false
   }
 
   override fun onDestroyView() {
@@ -56,6 +74,10 @@ class NotificationJustificationBottomSheet : BottomSheetDialogFragment() {
   }
 
   companion object {
-    fun create() = NotificationJustificationBottomSheet()
+    private const val REDIRECT_SETTINGS = "redirect_settings"
+
+    fun create(isRedirectSettings: Boolean) = NotificationJustificationBottomSheet().apply {
+      arguments = bundleOf(REDIRECT_SETTINGS to isRedirectSettings)
+    }
   }
 }
