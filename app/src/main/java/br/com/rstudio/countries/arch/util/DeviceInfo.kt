a1fbc69d7ab6501974374c30993b53b0414abb1f
Raphael <rodrigues.rph@gmail.com>
2025-10-06 20:27:25 -0300

    chore(#103; App): Update target SDK 35 (#104)
    * update target sdk, gradle and AGP
    
    * config java 17 on CI

--- app/src/main/java/br/com/rstudio/countries/arch/util/DeviceInfo.kt
+++ app/src/main/java/br/com/rstudio/countries/arch/util/DeviceInfo.kt
@@ -25,25 +25,23 @@ class DeviceInfo {
 
     val isEmulator: Boolean
       get() {
-        val locale = Locale.getDefault()
-
         return (
           Build.MANUFACTURER.contains("Genymotion")
             || Build.MODEL.contains("google_sdk")
-            || Build.MODEL.toLowerCase(locale).contains("droid4x")
+            || Build.MODEL.lowercase().contains("droid4x")
             || Build.MODEL.contains("Emulator")
             || Build.MODEL.contains("robolectric")
             || Build.MODEL.contains("Android SDK built for x86")
             || Build.HARDWARE == "goldfish"
             || Build.HARDWARE == "vbox86"
-            || Build.HARDWARE.toLowerCase(locale).contains("nox")
+            || Build.HARDWARE.lowercase().contains("nox")
             || Build.FINGERPRINT.startsWith("generic")
             || Build.PRODUCT == "sdk"
             || Build.PRODUCT == "google_sdk"
             || Build.PRODUCT == "sdk_x86"
             || Build.PRODUCT == "vbox86p"
-            || Build.PRODUCT.toLowerCase(locale).contains("nox")
-            || Build.BOARD.toLowerCase(locale).contains("nox")
+            || Build.PRODUCT.lowercase().contains("nox")
+            || Build.BOARD.lowercase().contains("nox")
             || (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic"))
           )
       }
